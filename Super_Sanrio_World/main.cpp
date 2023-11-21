370f92457b19509693414e7cbd8976d9e72913d4
dinmoy <imhyojin614@gmail.com>
2023-11-22 08:32:09 +0900

    [Fix] 음악 안나옴 오류 해결

--- Super_Sanrio_World/main.cpp
+++ Super_Sanrio_World/main.cpp
@@ -1,7 +1,17 @@
 #include "gamePage.h"
+#include<SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
-int main1(void)
+
+using namespace sf;
+
+int main(void)
 {
+    Music music;
+    if (!music.openFromFile("resources/sanrio_world_bgm.ogg"))
+        return -1; // error
+    music.play();
+
     gamePage gameP;
     gameP.run();
     return 0;
